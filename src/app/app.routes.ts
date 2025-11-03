9510136b6720a1731abc8f3018719b3f4aab0708
stijnpotters <stijn.potters1@gmail.com>
2025-11-03 16:55:32 +0100

    added pipeline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: '**', component: NotFoundComponent }
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "not-found", component: NotFoundComponent },
+  { path: "**", redirectTo: "/not-found" },
 ];
