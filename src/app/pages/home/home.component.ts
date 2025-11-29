a0c0d9c25d835f96d16554425bd85ba91e821919
stijnpotters <stijn.potters1@gmail.com>
2025-11-29 22:48:13 +0100

    refactor: update home section to include inspiring connections with slideshow functionality

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,24 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class HomeComponent implements OnInit, OnDestroy {
   scrollEnabled = false;
 
+  slideshows = [
+    { images: ['/assets/connecties-1.png'], currentIndex: 0 },
+    { images: ['/assets/connecties-2.png'], currentIndex: 0 },
+    { images: ['/assets/connecties-3.png'], currentIndex: 0 }
+  ];
+
+  connectiesTitles = [
+    'Surfkamp',
+    'House of Waves',
+    'AM.CLUB.RTM'
+  ];
+
+  connectiesDescriptions = [
+    'Ik heb voor Enter the Wave 3,5 weken les gegeven in Noord-Spanje.',
+    'Tijdens het event van House of Waves heb ik 2 super leuke yoga mogen verzorgen.',
+    'Tijdens het event van AM.CLUB.RTM heb ik een leuke power yoga les gegeven.'
+  ];
+
   ngOnInit() {
     const hasVisitedContent = localStorage.getItem('hasVisitedContent');
     const isAtTop = window.scrollY === 0;
@@ -37,5 +55,19 @@ export class HomeComponent implements OnInit, OnDestroy {
         aboutSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
       }
     }, 100);
+  }
+
+  setSlide(slideshowIndex: number, imageIndex: number) {
+    this.slideshows[slideshowIndex].currentIndex = imageIndex;
+  }
+
+  nextSlide(slideshowIndex: number) {
+    const slideshow = this.slideshows[slideshowIndex];
+    slideshow.currentIndex = (slideshow.currentIndex + 1) % slideshow.images.length;
+  }
+
+  prevSlide(slideshowIndex: number) {
+    const slideshow = this.slideshows[slideshowIndex];
+    slideshow.currentIndex = (slideshow.currentIndex - 1 + slideshow.images.length) % slideshow.images.length;
   }
 }
